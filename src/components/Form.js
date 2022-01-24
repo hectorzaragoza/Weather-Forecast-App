51f1c23a4dd9e77e20f37d020b2002ae0274f454
hectorzaragoza <zaragoza09@gmail.com>
2022-01-24 09:00:09 -0600

    added basic css

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,22 @@
+import '../index.css';
+
 const Form = (props) => {
     const { getUserInput, cityWeather } = props
     console.log('weather data in form component? ', cityWeather)
 
     const data = cityWeather.map((day, index) => {
         return (
-            <div key={index}>
+            <div key={index} className="dayCard">
                 <h1>Day: {index}</h1>
-                <h2>Min: {day.temp.min} C</h2>
-                <h2>Max: {day.temp.max} C</h2>
+                <h2>Min: {day.temp.min}°C</h2>
+                <h2>Max: {day.temp.max}°C</h2>
             </div>
         )
     })
 
     return (
         <>
-        <h2>Let's check out the weather forecast for your next vacation spot!</h2>
+        <h1>Let's check out the weather forecast for your next destination!</h1>
         <form onSubmit={getUserInput}>
             <label>
                 Enter City:
