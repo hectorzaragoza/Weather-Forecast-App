1a004c84bfd9238c0701335f3e874ec72a224380
hectorzaragoza <zaragoza09@gmail.com>
2022-01-24 10:10:16 -0600

    testing new remote url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename='https://hectorzaragoza.github.io/weather-app-mex/'>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
